c54703199c131f42b6f8f86701d1a29c8d7935ff
DKY10000 <374879250@qq.com>
2018-08-05 17:15:24 +0800

    实体字段改驼峰

--- src/main/java/com/dky/eureka/domain/user/entity/User.java
+++ src/main/java/com/dky/eureka/domain/user/entity/User.java
@@ -1,30 +1,72 @@
 package com.dky.eureka.domain.user.entity;
 
-        import io.swagger.annotations.ApiModel;
-        import io.swagger.annotations.ApiModelProperty;
-        import lombok.AllArgsConstructor;
-        import lombok.Builder;
-        import lombok.Data;
-        import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
-        import javax.persistence.*;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@ApiModel(value = "ChanceMaster")
 @Table(name = "sys_user")
 public class User {
     @Id
-    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @ApiModelProperty(value = "登陆人编号")
-    @Column(name = "fCode")
-    private String fcode;
+    /**
+     * 用户代码
+     */
+    @Column(name = "user_code")
+    private String userCode;
 
-    @ApiModelProperty(value = "登陆人名称")
-    @Column(name = "fName")
-    private String fname;
+    /**
+     * 用户名称
+     */
+    @Column(name = "user_name")
+    private String userName;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取用户代码
+     *
+     * @return user_code - 用户代码
+     */
+    public String getUserCode() {
+        return userCode;
+    }
+
+    /**
+     * 设置用户代码
+     *
+     * @param userCode 用户代码
+     */
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    /**
+     * 获取用户名称
+     *
+     * @return user_name - 用户名称
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * 设置用户名称
+     *
+     * @param userName 用户名称
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
--- src/main/java/com/dky/eureka/persistence/user/UserMapper.java
+++ src/main/java/com/dky/eureka/persistence/user/UserMapper.java
@@ -1,9 +1,7 @@
 package com.dky.eureka.persistence.user;
 
 import com.dky.eureka.domain.user.entity.User;
-import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
-@Repository
 public interface UserMapper extends Mapper<User> {
 }
